cc432249c9e2deb8bd80515e37480c9fae178dbc
TmAdminkafawang <ztjs777777@gmail.com>
2025-10-09 19:01:35 +0800

    feat:新代码上传

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -74,7 +74,7 @@ function go(act) {
     onMounted (()=>{
         if(!loginCookie.value){
             alert("请先登录管理员帐号")
-            router.push({ name: '/' })
+            router.push({ name: '/log' })
         }
     })
 </script>
