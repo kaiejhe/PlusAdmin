b10f214f80adda709d26fa0dcf06994abd475372
TmAdminkafawang <ztjs777777@gmail.com>
2025-10-11 14:54:00 +0800

    feat:新代码上传

--- functions/api/functions.js
+++ functions/api/functions.js
@@ -109,7 +109,7 @@ async function getlist(request,env) {
     const sql = `SELECT * FROM ${table} ${whereClause} LIMIT ?, ?`;
     try {
         // 查询总数量
-        const countRes = await db.prepare(countSql).bind(...values).first();
+        const countRes = await db.prepare(sql).bind(...values).first();
         const total = countRes?.total || 0;
         // 查询当前页数据
         const res = await db.prepare(sql).bind(...values, offset, pageSize).all();
