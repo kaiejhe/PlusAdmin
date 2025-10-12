98e95cb0fa9945caa4c63f526d269a1d1f8e0bc0
TmAdminkafawang <ztjs777777@gmail.com>
2025-10-13 03:31:01 +0800

    feat:新代码上传

--- functions/api/functions.js
+++ functions/api/functions.js
@@ -240,7 +240,7 @@ export async function AdminToken(request, env){
       .bind(orderId, Email, Cardcode, Token, "o1", timestamp)
       .run();
     if (orderInsert.meta.last_row_id<1) {
-      return json({ ok: false, msg:res }, 500);
+      return json({ ok: false, msg:orderInsert }, 500);
     }
     return json({ ok: true, msg: "Plus订阅任务提交成功" }, 200);
   } catch (error) {}
