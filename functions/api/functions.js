09a645e13ea21a74247226e299a41961b152ab9f
TmAdminkafawang <ztjs777777@gmail.com>
2025-10-18 14:42:56 +0800

    feat:新代码上传

--- functions/api/functions.js
+++ functions/api/functions.js
@@ -308,7 +308,7 @@ export async function TeamEmail(request, env){
     try {
       const chinaTime = new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Shanghai" })).getTime();
       
-      const T3 =  await db.prepare(`INSERT INTO teamorder (usEmail, accEmail, orTime, "State", created_at) VALUES (?, ?, ?, ?, ?)`)
+      const T3 =  await db.prepare(`INSERT INTO teamorder (usEmail, accEmail, orTime, State, created_at) VALUES (?, ?, ?, ?, ?)`)
           .bind(Email, TeamRES.accEmail, CardRes.CardTime, "o2", chinaTime).run()
   
       return json({ ok: true, msg: "已成功发送邀请,请留意邮件",T3:T3}, 200); 
