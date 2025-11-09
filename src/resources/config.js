8613f7e5317b4451408a954be172c410aa218f53
TmAdminkafawang <ztjs777777@gmail.com>
2025-11-09 23:37:01 +0800

    Initial commit

--- src/resources/config.js
+++ src/resources/config.js
@@ -101,11 +101,9 @@ export const RESOURCE_CONFIG = {
       'AfterSales',
       'TeamOrderState',
       'TeamCard',
-      'TeamNum',
     ],
     defaults: {
       TeamOrderState: 'o1',
-      TeamNum: 0,
       AfterSales: 0,
     },
   },
